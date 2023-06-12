70a54fbfac8f169a51652506b480f98a14f6c1db
Decxo360 <diegolundstedt99@gmail.com>
2023-06-12 19:22:58 -0400

    fix:cambio de atributos

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,12 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
 import { config } from "dotenv";
 
 config()
 
 export const client = new DynamoDB({
-    region:"process.env.AWS_REGION",
+    region: process.env.AWS_REGION,
     credentials:{
         accessKeyId: process.env.DIEGO_ACCESS_KEY,
-        secretAccessKey:process.env.DIEGO_ACCESS_SECRET
+        secretAccessKey:process.env.DIEGO_ACCESS_SECRET_KEY
     }
 })
