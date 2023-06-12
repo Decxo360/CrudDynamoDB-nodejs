493b5bd3b538799b161280e47ba89085ebd1538f
Decxo360 <diegolundstedt99@gmail.com>
2023-06-12 19:22:16 -0400

    Feat: importacion de rutas

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import  express  from "express";
-
+import express from "express";
+import UsuarioRouter from './src/router/UsuarioRouter.js'
 
 
 const app = express()
 
+app.use(express.json())
+
+app.use('/usuario', UsuarioRouter)
 
 app.listen(3030,()=>{
     console.log('RUNNIN ON PORT 3030');
