f5e3b9e10631023d72663f8ec613913029ff53ba
Decxo360 <diegolundstedt99@gmail.com>
2023-06-19 18:07:01 -0400

    feat: encriptacion de contraseña

--- src/service/UsuarioService.js
+++ src/service/UsuarioService.js
@@ -1,16 +1,16 @@
 import { v4 } from "uuid";
 import { client } from "../config/config.js"
+import { genSaltSync,hashSync } from "bcrypt";
 import { ScanCommand, PutItemCommand, DeleteItemCommand } from "@aws-sdk/client-dynamodb"
 
 class UsuarioService{
     async getUsuario(data){
-        console.log(data);
         const params = {
             TableName: 'Usuario',
-            FilterExpression: 'email = :email AND contrasena = :contrasena',
+            FilterExpression: 'email = :email',
             ExpressionAttributeValues:{
                 ':email' : {S:data.correo},
-                ':contrasena' : {S:data.contrasena}
+                // ':contrasena' : {S:data.contrasena}
             }
         }
         const commnad = new ScanCommand(params)
@@ -19,7 +19,10 @@ class UsuarioService{
         
     }
     async createUsuario(data){
-        
+
+        const salt = genSaltSync();
+        const contrasenaEncriptada = hashSync(data.contrasena,salt);
+
         const params ={
             TableName:"Usuario",
             Item: {
@@ -27,13 +30,13 @@ class UsuarioService{
                 nombre: {S:data.nombre},
                 apellido:{S:data.apellido},
                 email:{S:data.email},
-                contrasena:{S:data.contrasena},
+                contrasena:{S:contrasenaEncriptada},
                 nick:{S:data.nick}
             }
         }
+
         const command = new PutItemCommand(params)
         const result = await client.send(command)
-        console.log(result);
         return result
     }
 
